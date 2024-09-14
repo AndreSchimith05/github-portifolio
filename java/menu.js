d9ed0628be7af3a43dbc60d465348578a21e07fd
AndreSchimith05 <andreschimith05@gmail.com>
2024-09-13 23:21:00 -0300

    Add arquivos

---
+++ java/menu.js
@@ -0,0 +1,14 @@
+var burguer = document.querySelector('burguer')
+function abrir(){
+    if(menu.style.display=='block'){
+        menu.style.display='none'
+    }else{
+        menu.style.display='block'
+    }
+}
+function mudouTamanho(){
+    if (window.innerWidth>= 768){
+    menu.style.display='block'} else{
+        menu.style.display ='none'
+    }
+}
